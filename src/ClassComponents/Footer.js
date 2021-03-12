bc5d23d522488a9ab82edf146b19d78dd7f66764
Farid <farid.adnaoui@outlook.fr>
2021-03-12 23:34:53 +0100

    Correction syntaxique du code & rajouts d'images

--- src/ClassComponents/Footer.js
+++ src/ClassComponents/Footer.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../Css/Footer.css';
 
-class Footer extends React.Component{
+class Footer extends Component{
     render(){
         return(
             <div class="Footer">Je suis le footer</div>
