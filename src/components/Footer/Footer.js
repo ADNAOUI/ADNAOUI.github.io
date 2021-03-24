57f5e54dd38c1ce72ebebb163ee6312bc081d216
@ADNAOUI <farid.adnaoui@outlook.fr>
2021-03-24 23:13:29 +0100

    commit sur toutes les modifs et non a partir dun dossier spécifique

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 import '../../assets/css/Footer.css';
+import { NavLink } from "react-router-dom";
+
+//Images
 import LogoMs_Lab from '../../assets/img/logoMs_Lab.png';
 import Logo_Ministere from '../../assets/img/logoMinistere.png';
 import Logo from '../../assets/img/logoRessourcesRelationnelles.png';
+
 export default class Footer extends Component{
+    constructor(props) {
+        super(props);
+        this.activeRoute.bind(this);
+      }
+      // On vérifie si le route existe ou non
+      activeRoute(routeName) {
+        return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+      }
+
     render()
     {
         return(
@@ -14,14 +27,30 @@ export default class Footer extends Component{
                     <img src={Logo_Ministere} class="imgFooter" alt="Logo du ministère"/>
                 </div>
 
-                <div>
-                    <label class="titleFooter">AIDES</label>
-                    <label class="titleFooter">FAQ</label>
-                    <label class="titleFooter">A PROPOS</label>
-                    <label class="titleFooter">CONDITIONS</label>
-                    <label class="titleFooter">POLITIQUES DE COOKIES</label>
-                    <label class="titleFooter">LIGNES DE CONDUITES</label>
-                </div>
+                <ul>
+                    {this.props.routesFooter.map((prop, key) => {
+                        if (prop.redirect) return null;
+                            return (
+                                <li
+                                    className={
+                                        this.activeRoute(prop.layoutFooter + prop.pathFooter) +
+                                        (prop.pro ? "active active-pro" : "")
+                                    }
+                                    key={key}
+                                    >
+
+                                    <NavLink
+                                            to={prop.layoutFooter + prop.pathFooter}
+                                            className="nav-link"
+                                            activeClassName="active"
+                                    >
+                                        <label>{prop.nameFooter}</label>
+                                    </NavLink>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
 
                 <div>
                     <p class="textFooter">&copy; 2021 Ressources Relationnelles Tout droit réservé</p>
