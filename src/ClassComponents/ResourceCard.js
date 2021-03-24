1093b939ff43570cbf978fdcf2a9b10b647744b0
Jimmy_B <benard.jimmy36@gmail.com>
2021-03-24 23:24:55 +0100

    Test destinataire commit

--- src/ClassComponents/ResourceCard.js
+++ src/ClassComponents/ResourceCard.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class ResourceCard extends Component{
     render(){
         return(
-            <h1>Je suis la ResourceCard</h1>
+            <h1>Je suis la ResourceCard 2</h1>
         );
     }
 }
