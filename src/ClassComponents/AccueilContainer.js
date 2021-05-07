c0fd0cec70d12ca71716fa5f6003698ac816e2cb
Jimmy_B <benard.jimmy36@gmail.com>
2021-05-07 07:45:38 +0200

    Fonctionnalités AccueilContainer

--- src/ClassComponents/AccueilContainer.js
+++ src/ClassComponents/AccueilContainer.js
@@ -1,82 +1,116 @@
-//----- MODULES -----//
-import React, { Component } from 'react';
+//*----- MODULES -----//
+import React, { Component }           from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Axios from 'axios';
+import Axios                          from 'axios';
 
-//----- COMPOSANTS -----//
-import ResourceContainer from '../views/ResourceContainer.js';
-import ResourceCard from './ResourceCard.js';
+//*----- COMPOSANTS -----//
+import ResourceContainer              from '../views/ResourceContainer.js';
+import ResourceCard                   from './ResourceCard.js';
 
-//----- STYLES -----//
+//*----- STYLES -----//
 import '../assets/css/view/AccueilContainer.css';
 
+/**
+ *  @class AccueilContainer est la première page que les utilisateurs verront. Elle est également la page de redirection par défaut.
+ *                          Elle gère l'ensemble des ressources publiées.
+ **/
 class AccueilContainer extends Component {
-  constructor(props){
+  //*----- INSTANCIATION -----//
+  constructor(props) {
     super(props);
     this.state = {
-      //----- STATES BdB -----//
-      ressources : [],
+      /**--- DETAILS VARIABLES
+       *  @property {array}   ressources   Données de la table 'ressources' de l'API
+       *  @property {boolean} loading      Comportement du chargement
+       *  @property {int}     page         Numéro de page actuelle de l'API
+       *  @property {int}     prevY        Numéro de la précédente page
+       **/
+      //----- STATES BdD
+        ressources: [],
 
-      //----- STATES LOCALES -----//
-      page: 1
+      //----- STATES LOCALES
+      loading: false,
+      page   : 1,
+      prevY  : 0
     }
   }
 
-  fetchData = (pageNum) => {
-    // Axios ({
-    //   method: 'get',
-    //   url: 'http://localhost:8055/items/ressources',
-    //   responseType: 'json',
-    // }).then(response => {
-    //     const ressources = response.data.data
-    //     this.setState({ressources});
-    //     console.log(ressources)
-    // });
-    let ressourceUrl = 'http://localhost:8055/items/ressources?page=' + pageNum;
-    console.log(ressourceUrl);
-    console.log(pageNum);
-
-    fetch (ressourceUrl)
-      .then(response => response.json())
-      .then(data => {
-          this.setState({
-            ressources: [...this.state.ressources, [data]]
-          })
-          console.log(data)
-      })
+  //*----- FONCTIONS -----//
+  /**
+   *  @function getRessources contient le code source utilisé pour obtenir les données de l'API.
+   *  @param {Int} page contient le numéro de la page actuelle de l'API.
+   **/
+  getRessources = (page) => {
+    console.log("page", page)
+    this.setState({ loading: true }); //....................................................Démarrage du chargement des données de l'API
+    Axios({
+      method:       'get',
+      url:          `http://localhost:8055/items/ressources?page=${page}&limit=20`, //......URL de l'API avec mise à jour du numéro de la page reçue en paramètre.
+      responseType: 'json'
+    }).then(response => {
+        this.setState({ ressources: [...this.state.ressources, ...response.data.data] }); //Stockage de la réponse de l'API dans le tableau posts
+        this.setState({ loading: false }); //...............................................Arrêt du chargement des données de l'API
+    })
   }
 
-  infiniteScroll = () => {
-    //Bas de page atteint ?
-    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
-      let newPage = this.state.page;
-      newPage ++;
-
-      this.setState({
-        page: newPage
-      })
-
-      this.fetchData(newPage);
+  /**
+   *  @function handleObserver onfigure le numéro de la page en fonction de l'élément cible et mémorise la dernière page en cours de défilement.
+   *  @param {IntetersectionObserverEntry object} entities
+   *  @param {object} observer
+   **/
+  handleObserver = (entities, observer) => {
+    const y = entities[0].boundingClientRect.y; /*Renvoie un objet DOMRect permettant d'avoir la taille d'un élément et sa position par rapport à la fenêtre.
+    y est une des ses propriétés décrivant la position de la taille du rectangle en pixel*/
+    if(this.state.prevY > y) {
+      const lastRessource = this.state.ressources[this.state.ressources.length - 1];
+      const curPage = this.state.page + 1;
+      this.getRessources(curPage);
+      this.setState({ page: curPage });
+      console.log("CurPage", curPage)
     }
+    this.setState({ prevY: y });
   }
 
-  componentDidMount = () => {
-    window.addEventListener('scroll', this.infiniteScroll);
-    this.fetchData (this.state.page);
-}
+  /**
+   *  @function componentDidMount permet d'exécuter des fonctions une fois les composants chargés.
+   **/
+  componentDidMount(){
+    this.getRessources(this.state.page); //..........................................Envoi du numéro de la page actuelle à mettre à jour
+    var options = {
+      root: null, //.................................................................Racine à utiliser pour l'intersection
+      rootMargin: "0px", //..........................................................Valeur de marge de la racine
+      threshold: 1.0 /*..............................................................Seuil de l'écran : valeur utilisée pour déclencher le rappel une fois que la zone de
+                                                                                     l'intersection est supérieure ou identique causé par le scroll*/
+    };
+    this.observer = new IntersectionObserver(this.handleObserver, options); /*.......Observateur d'intersection permettant d'écouter les changements de l'élément cible
+                                                                                     (param : Le nom de la fonction de rappel d'observateur, les options supplémentaires,
+                                                                                     telles que la racine et le seuil)*/
+    this.observer.observe(this.loadingRef);
+  }
 
   render(){
+    //*----- MAPPING -----//
+
+    //*----- CSS CONDITIONNEL -----//
+    /**
+     *  @const loadingTextCSS Affiche le message du chargement en fonction de l'état.
+     **/
+    const loadingTextCSS = { display: this.state.loading ? "block" : "none" };
     return(
       <div>
         <BrowserRouter>
           <div className="div_ResourceContainer">
-          {/*AFFICHER toutes les ressources cards de la BdD*/}
-            {this.state.ressources.map((ressourceData, index) => (
-              <Link to="/ressources_relationnelles/resourcecontainer">#{index}<ResourceCard key={index} ressource={ressourceData}/></Link>
-            ))}
-            {/* <ul className="ul_ResourceContainer">
-                {this.state.ressources.map(ressource => <li><Link to="/ressources_relationnelles/resourcecontainer"><ResourceCard /></Link></li>)}
-            </ul> */}
+            {/*AFFICHER toutes les ressources cards de la BdD*/}
+            <ul>
+              {this.state.ressources.map((ressource, indexRessource) =>
+                <Link to="/ressources_relationnelles/resourcecontainer"><ResourceCard key={indexRessource} tableRessources={ressource}/></Link>
+              )}
+            </ul>
+          </div>
+
+          <div className="loadingCSS"
+               ref={loadingRef => (this.loadingRef = loadingRef)}>
+            <span style={loadingTextCSS}>Chargement...</span>
           </div>
 
           <div className="div_ResourceContent">
--- src/ClassComponents/ResourceCard.js
+++ src/ClassComponents/ResourceCard.js
@@ -1,9 +1,9 @@
-//----- MODULES -----//
+//*----- MODULES -----//
 import React, { Component } from 'react';
 import { Card }             from 'react-bootstrap';
 import Axios                from 'axios';
 
-//----- MULTIMEDIAS -----//
+//*----- MULTIMEDIAS -----//
 import resourcesImg         from '../assets/img/resourcesImg.png';
 import resourceCardAvatar   from '../assets/img/resourceCardAvatar.png';
 import heartIcon            from '../assets/img/heartIcon.png';
@@ -11,37 +11,38 @@ import commentIcon          from '../assets/img/commentIcon.png';
 import shareIcon            from '../assets/img/shareIcon.png';
 import starIcon             from '../assets/img/starIcon.png';
 
-//----- STYLES -----//
+//*----- STYLES -----//
 import '../assets/css/view/ResourceCard.css';
 
-class ResourceCard extends Component{
+class ResourceCard extends Component {
     constructor(props){
         super(props);
         this.state = {
-            //----- STATES BdD -----//
-            ressources                  : Array.from({ length: 10 }),
+            //*----- STATES BdD -----//
+            //------ MEMBRES
+            membres: [],
+
+            //------ RESSOURCES
+            ID_RESSOURCE              : props.tableRessources.ID_RESSOURCE,
+            TITRE_RESSOURCE           : props.tableRessources.TITRE_RESSOURCE,
+            DATE_PUBLICATION_RESSOURCE: props.tableRessources.DATE_PUBLICATION_RESSOURCE,
+            IMAGE_RESSOURCE           : props.tableRessources.IMAGE_RESSOURCE,
 
             //----- STATES LOCALES -----//
-            nombreCommentaireRessource  : 0,
-            nombrePartageRessource      : 0,
-            nombreLikeRessource         : 0,
-            pseudonymeMembreResourceCard: 'John Doe',
-            professionMembreResourceCard: 'Développeur informatique',
-            visible                     : true,
+            nombreCommentaireRessource: 0,
+            nombrePartageRessource    : 0,
+            nombreLikeRessource       : 0,
+            visible                   : true,
         };
     }
 
     componentDidMount(){
-        Axios ({
+        Axios({
             method: 'get',
-            url: 'http://localhost:8055/items/ressources',
-            responseType: 'json',
-            params : {
-                limit: 999999999
-            }
+            url: 'http://localhost:8055/items/membres',
+            responseType: 'json'
         }).then(response => {
-            const ressources = response.data.data
-            this.setState({ressources});
+            this.setState({ membres: [...this.state.membres, ...response.data.data] });
         });
     }
 
@@ -50,10 +51,12 @@ class ResourceCard extends Component{
     }
 
     render(){
+        console.log("membres :", this.state.membres)
         return(
             <div id="resourceCard" className="resourceCard">
+                #{this.state.ID_RESSOURCE}
                 <div id="lbl_publicationResourceCard" className="col-12 lbl_publicationResourceCard">
-                    <span id="span_publicationResourceCard" className="span_publicationResourceCard">publié le : <strong>15/05/2020</strong></span>
+                    <span id="span_publicationResourceCard" className="span_publicationResourceCard">publié le : <strong>{this.state.DATE_PUBLICATION_RESSOURCE}</strong></span>
                 </div>
 
                 <div className="cardRessource">
@@ -61,10 +64,10 @@ class ResourceCard extends Component{
                 <div className="cardBody">
                     <Card.Body style={{padding: 10, backgroundColor: "#F7F7F7", borderTopRightRadius: 18}}>
                         <div className="row titleResourceCard">
-                            <Card.Title><strong className="tailleTitreCardRessources">{this.state.titreRessource}</strong></Card.Title>
+                            <Card.Title><strong className="tailleTitreCardRessources">{this.state.TITRE_RESSOURCE}</strong></Card.Title>
                         </div>
                         <div className="row contentPictureResourceCard">
-                            <Card.Img style={{alignItems: 'center'}} src={resourcesImg}/>
+                            <Card.Img style={{alignItems: 'center'}} src={this.state.IMAGE_RESSOURCE} alt=""/>
                         </div>
                         <div className="colorCardBadge">
                             <span className="badgeRessourceCard" variant="primary">1</span>
@@ -73,7 +76,8 @@ class ResourceCard extends Component{
                         </div>
                         <div className="row ProfilPictureResourceCard">
                             <Card.Img style={{width: '40px'}} src={resourceCardAvatar}/>
-                            <span className="span_PseudonymeMembreResourceCard"><strong>{this.state.pseudonymeMembreResourceCard}</strong></span>
+                            {this.state.membres.map((indexMembre, membre) => <span className="span_PseudonymeMembreResourceCard" key={indexMembre}><strong>{membre.NOM_UTILISATEUR_MEMBRE}</strong></span>)}
+
                             <span className="span_ProfessionMembreResourceCard">{this.state.professionMembreResourceCard}</span>
                         </div>
                     </Card.Body>
